8379d75594682ec0b0bbe0d6133e714ec2bef30e
faizansolangi <faizansolangi576@gmail.com>
2023-12-14 10:04:10 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <AboutUs />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeroSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeroSection(){
+    return(
+        <>
+            <h1> Hero Section </h1>
+        </>
+    );
+}
+
+export default HeroSection;
